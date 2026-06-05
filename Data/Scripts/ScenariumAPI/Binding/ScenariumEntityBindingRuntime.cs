d087b1f2f4e7ad900657f5536b2b68e16dc66bb8
Daniel Hardy <Daniel.hardy@spray.com>
2026-06-05 10:08:31 +0200

    GameplayNodeBinding

--- Data/Scripts/ScenariumAPI/Binding/ScenariumEntityBindingRuntime.cs
+++ Data/Scripts/ScenariumAPI/Binding/ScenariumEntityBindingRuntime.cs
@@ -3,6 +3,8 @@ using VRage.Game.ModAPI;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ScenariumAPI.Runtime;
+using ScenariumAPI.Integrations.MES;
 
 namespace ScenariumAPI.Binding
 {
@@ -12,10 +14,33 @@ namespace ScenariumAPI.Binding
         readonly Action<string, string, bool> _transitionNode;
         readonly Action<string> _log;
 
+        CampaignRuntime _runtime;
+        MesBindingBridge _mesBridge;
+        ScenariumBindingInspectionTools _inspection;
+
+        int _lastScanMatched;
+        int _lastScanEntities;
+
         public ScenariumEntityBindingRuntime(Action<string, string, bool> transitionNode, Action<string> log)
         {
             _transitionNode = transitionNode;
             _log = log;
+        }
+
+        public ScenariumEntityBindingRuntime(CampaignRuntime runtime, Action<string> log, Action<string, string, bool> transitionNode)
+        {
+            _runtime = runtime;
+            _log = log;
+            _transitionNode = transitionNode;
+        }
+
+        public ScenariumEntityBindingRuntime(CampaignRuntime runtime, MesBindingBridge mesBridge, Action<string> log)
+        {
+            _runtime = runtime;
+            _mesBridge = mesBridge;
+            _log = log;
+            _transitionNode = null;
+            _inspection = new ScenariumBindingInspectionTools(_runtime, _mesBridge);
         }
 
         public int Count
@@ -26,6 +51,13 @@ namespace ScenariumAPI.Binding
         public ScenariumEntityBindingStore Store
         {
             get { return _store; }
+        }
+
+        public void SetContext(CampaignRuntime runtime, MesBindingBridge mesBridge)
+        {
+            _runtime = runtime;
+            _mesBridge = mesBridge;
+            _inspection = new ScenariumBindingInspectionTools(_runtime, _mesBridge);
         }
 
         public void Bind(long entityId, string nodeId, string spawnGroup, string gridName)
@@ -54,6 +86,37 @@ namespace ScenariumAPI.Binding
                 _log("Persistent entity bindings cleared.");
         }
 
+        public int ScanByCampaignBindings()
+        {
+            if (_inspection == null)
+                _inspection = new ScenariumBindingInspectionTools(_runtime, _mesBridge);
+
+            List<long> candidates = _inspection.GetCandidateEntityIds();
+            _lastScanEntities = candidates.Count;
+            _lastScanMatched = 0;
+
+            foreach (long entityId in candidates)
+            {
+                if (_store.Contains(entityId))
+                    continue;
+
+                string nodeId;
+                string spawnGroup;
+                string gridName;
+
+                if (_inspection.TryMatchEntity(entityId, out nodeId, out spawnGroup, out gridName))
+                {
+                    Bind(entityId, nodeId, spawnGroup, gridName);
+                    _lastScanMatched++;
+                }
+            }
+
+            if (_log != null)
+                _log("Entity binding scan complete. Candidates: " + candidates.Count + " Newly bound: " + _lastScanMatched);
+
+            return _lastScanMatched;
+        }
+
         public void Update()
         {
             List<ScenariumEntityBindingData> closed = new List<ScenariumEntityBindingData>();
@@ -80,6 +143,8 @@ namespace ScenariumAPI.Binding
 
                 if (_transitionNode != null)
                     _transitionNode(binding.NodeId, "destroyed", false);
+                else if (_log != null)
+                    _log("Bound entity transition requires command runtime callback. Node: " + binding.NodeId);
 
                 binding.TransitionApplied = true;
             }
@@ -88,6 +153,18 @@ namespace ScenariumAPI.Binding
         public string BuildSummary()
         {
             return _store.BuildSummary();
+        }
+
+        public string BuildDiagnostics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Persistent Entity Binding Runtime");
+            sb.AppendLine("Bindings: " + _store.Count);
+            sb.AppendLine("Last scan candidates: " + _lastScanEntities);
+            sb.AppendLine("Last scan newly bound: " + _lastScanMatched);
+            sb.AppendLine("");
+            sb.Append(_store.BuildSummary());
+            return sb.ToString();
         }
     }
 }
